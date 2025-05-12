2ba4dfe911a1bae44448a0bcce0cfdad7b1b28d0
abdullah-albanna <abdu.albanna@proton.me>
2025-05-12 13:20:06 +0300

    don't yank on delete

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,58 +1,67 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
--- map("i", "jk", "<ESC>")
+local function merge_opts(extra)
+  return vim.tbl_extend("force", opts, extra or {})
+end
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- DAP UI
+map("n", "<Leader>du", "<cmd>lua require'dapui'.toggle()<CR>", merge_opts { desc = "open up the dap ui" })
 
--- Nvim DAP UI
-map("n", "<Leader>du", "<cmd>lua require'dapui'.toggle()<CR>", { desc = "open up the dap ui" })
-
--- Nvim DAP
-map("n", "<Leader>dl", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Debugger step into" })
-map("n", "<Leader>dj", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Debugger step over" })
-map("n", "<Leader>dk", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Debugger step out" })
-map("n", "<Leader>dc", "<cmd>lua require'dap'.continue()<CR>", { desc = "Debugger continue" })
-map("n", "<Leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
+-- DAP
+map("n", "<Leader>dl", "<cmd>lua require'dap'.step_into()<CR>", merge_opts { desc = "Debugger step into" })
+map("n", "<Leader>dj", "<cmd>lua require'dap'.step_over()<CR>", merge_opts { desc = "Debugger step over" })
+map("n", "<Leader>dk", "<cmd>lua require'dap'.step_out()<CR>", merge_opts { desc = "Debugger step out" })
+map("n", "<Leader>dc", "<cmd>lua require'dap'.continue()<CR>", merge_opts { desc = "Debugger continue" })
+map(
+  "n",
+  "<Leader>db",
+  "<cmd>lua require'dap'.toggle_breakpoint()<CR>",
+  merge_opts { desc = "Debugger toggle breakpoint" }
+)
 map(
   "n",
   "<Leader>dd",
   "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
-  { desc = "Debugger set conditional breakpoint" }
+  merge_opts { desc = "Debugger set conditional breakpoint" }
 )
+map("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>", merge_opts { desc = "Debugger reset" })
+map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", merge_opts { desc = "Debugger run last" })
 
-map("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
-map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
+-- moving lines
+map("n", "<A-C-k>", ":m .-2<CR>==", opts)
+map("n", "<A-C-j>", ":m .+1<CR>==", opts)
+map("v", "<A-C-k>", ":m '<-2<CR>gv=gv", opts)
+map("v", "<A-C-j>", ":m '>+1<CR>gv=gv", opts)
 
--- moving the line up and down
-map("n", "<A-C-k>", ":m .-2<CR>==", { noremap = true, silent = true })
-map("n", "<A-C-j>", ":m .+1<CR>==", { noremap = true, silent = true })
+-- LSP
+map("n", "<Leader>ca", vim.lsp.buf.code_action, merge_opts { desc = "code action" })
+map("n", "gd", vim.lsp.buf.definition, merge_opts { desc = "goto definition" })
+map("n", "gD", vim.lsp.buf.declaration, merge_opts { desc = "goto declaration" })
+map("n", "gi", vim.lsp.buf.implementation, merge_opts { desc = "goto implementation" })
+map("n", "gr", vim.lsp.buf.references, merge_opts { desc = "goto references" })
+map("n", "<Leader>rn", vim.lsp.buf.rename, merge_opts { desc = "rename" })
 
-map("v", "<A-C-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
-map("v", "<A-C-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+map("n", "E", vim.diagnostic.open_float, opts)
 
--- LSP-related keymaps
-map("n", "<Leader>ca", vim.lsp.buf.code_action, { noremap = true, silent = true, desc = "code action" })
-map("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true, desc = "goto definition" })
-map("n", "gD", vim.lsp.buf.declaration, { noremap = true, silent = true, desc = "goto declaration" })
-map("n", "gi", vim.lsp.buf.implementation, { noremap = true, silent = true, desc = "goto implementation" })
-map("n", "gr", vim.lsp.buf.references, { noremap = true, silent = true, desc = "goto references" })
-map("n", "<Leader>rn", vim.lsp.buf.rename, { noremap = true, silent = true, desc = "rename" })
+map("n", "<Leader>td", "<cmd>TodoTelescope<CR>", merge_opts { desc = "open todos" })
 
--- open up the full error
-map("n", "E", vim.diagnostic.open_float)
-
--- opens up a telescope of todos
-map("n", "<Leader>td", "<cmd>TodoTelescope<CR>", { noremap = true, silent = true, desc = "open todos" })
-
--- toggle inlay hints
 map("n", "<Leader>ih", function()
   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-end, { desc = "Toggle inlay hints", noremap = true, silent = true })
+end, merge_opts { desc = "Toggle inlay hints" })
 
-map("n", "j", "gj", { noremap = true, silent = true })
-map("n", "k", "gk", { noremap = true, silent = true })
+-- Movement tweaks
+map("n", "j", "gj", opts)
+map("n", "k", "gk", opts)
+
+-- Don't yank on delete/change
+map("n", "d", '"_d', opts)
+map("n", "D", '"_D', opts)
+map("n", "c", '"_c', opts)
+map("n", "C", '"_C', opts)
+map("n", "x", '"_x', opts)
+map("n", "X", '"_X', opts)
+map("v", "d", '"_d', opts)
+map("v", "x", '"_x', opts)
